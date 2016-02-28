a6f336f8b80bb4feb5d27f7dae20cf61366e0377
Connor Shearwood <drake@lifein2d.co.uk>
2016-02-28 18:44:12 +0000

    Add encounter 1 map first draft

---
+++ tilesets/complex.tsx
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Complex" tilewidth="129" tileheight="152" tilecount="13" columns="0">
+ <tile id="0">
+  <image width="129" height="68" source="../images/tiles/bed.png"/>
+ </tile>
+ <tile id="1">
+  <image width="129" height="108" source="../images/tiles/bunk.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="69" source="../images/tiles/crates1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="85" source="../images/tiles/crates2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="129" height="78" source="../images/tiles/desk1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="78" source="../images/tiles/desk2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="48" source="../images/tiles/floor1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="48" source="../images/tiles/floor2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="48" source="../images/tiles/grate.png"/>
+ </tile>
+ <tile id="9">
+  <image width="129" height="89" source="../images/tiles/medbay-bed.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="69" source="../images/tiles/table.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="152" source="../images/tiles/wall.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="72" source="../images/tiles/short-wall.png"/>
+ </tile>
+</tileset>
---
+++ tilesets/rainforest.tsx
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Rainforest" tilewidth="128" tileheight="90" tilecount="21" columns="0">
+ <terraintypes>
+  <terrain name="Path" tile="-1"/>
+ </terraintypes>
+ <tile id="0">
+  <image width="128" height="48" source="../images/tiles/grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="90" source="../images/tiles/veg1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="48" source="../images/tiles/water.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="48" source="../images/tiles/pool1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="48" source="../images/tiles/pool2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="48" source="../images/tiles/pool3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="48" source="../images/tiles/pool4.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="48" source="../images/tiles/pool5.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="48" source="../images/tiles/pool6.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="48" source="../images/tiles/pool7.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="48" source="../images/tiles/pool8.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="48" source="../images/tiles/pool9.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="48" source="../images/tiles/path1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="48" source="../images/tiles/path2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="48" source="../images/tiles/path3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="48" source="../images/tiles/path4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="48" source="../images/tiles/path5.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="48" source="../images/tiles/path6.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="48" source="../images/tiles/path7.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="48" source="../images/tiles/path8.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="48" source="../images/tiles/path9.png"/>
+ </tile>
+</tileset>
