2ef1f6a38fab18b4f1239c3edb0cde9a7ef62a46
Connor Shearwood <drake@lifein2d.co.uk>
2016-02-29 16:03:47 +0000

    Tiles are being rendered in a sub-optinal fashion, there's some z-ordering issues and there's some vertical overdraw happening, but it works! You can now scroll around the map.

--- lostcolony/mapviewer.py
+++ lostcolony/mapviewer.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 class PygletTiledMap:
     def __init__(self, window, mapfile):
         self.camera = (0, 0)
+        self.camera_vector = (0, 0)
         self.tmx = pytmx.TiledMap(mapfile)
         self.window = window
         self.images = {}
@@ -22,7 +23,6 @@ class PygletTiledMap:
         self.images[name] = pyglet.image.load(path)
 
     def draw(self):
-        self.window.clear()
         for n, layer in enumerate(self.tmx.layers):
             for x, y, image_data in layer.tiles():
                 imgpath, _, _ = image_data
@@ -38,18 +38,48 @@ class PygletTiledMap:
                 cx, cy = self.camera
 
                 if cx - tilewidth < sx < cx + self.window.width + tilewidth * 2 and cy - tileheight < sy < cy + self.window.height + tileheight * 2:
-                    sprite = pyglet.sprite.Sprite(image, sx, self.window.height-sy)
+                    sprite = pyglet.sprite.Sprite(image, sx-cx, self.window.height-sy+cy)
                     sprite.draw()
 
 
 window = pyglet.window.Window(resizable=True)
+window.push_handlers(pyglet.window.event.WindowEventLogger())
 tmxmap = PygletTiledMap(window, "maps/encounter-01.tmx")
 
 
 @window.event
 def on_draw():
+    window.clear()
     tmxmap.draw()
 
+
+@window.event
+def on_mouse_motion(x, y, dx, dy):
+    mx, my = 0, 0
+    if x < window.width * 0.05:
+        mx = -1
+    elif x > window.width * 0.95:
+        mx = 1
+
+    if y < window.height * 0.05:
+        my = 1
+    elif y > window.height * 0.95:
+        my = -1
+
+    tmxmap.camera_vector = mx, my
+
+
+def update(_, dt):
+    print(_)
+    ox, oy = tmxmap.camera
+    dx, dy = tmxmap.camera_vector
+
+    nx = ox + dx * dt * 500
+    ny = oy + dy * dt * 500
+
+    tmxmap.camera = nx, ny
+
+pyglet.clock.schedule(update, 1/60)
 
 if __name__ == '__main__':
     pyglet.app.run()
