0a7651906bcc2a24e3fa942989938af6b553f440
nick.a.sarbicki@gmail.com <Nick.A.Sarbicki@gmail.com>
2016-03-02 09:54:57 +0000

    Subclassing animation instance for a characteranimation instance.
    Allows us to use a separate class for other animations, e.g. weapon fire. whereas characteranimations also hold name and directional information.

--- lostcolony/animation.py
+++ lostcolony/animation.py
@@ -10,22 +10,27 @@ DEFAULT_FRAME_RATE = 1
 
 loop = object()
 
+
 class Animation:
     def __init__(self, sequences, frame_rate=DEFAULT_FRAME_RATE):
         self.sequences = sequences
         self.frame_rate = frame_rate
 
-    def create_instance(self, character_name, pos=(0,0), direction='se'):
-        return AnimationInstance(self, character_name, pos, direction)
+    def create_instance(self, pos=(0,0)):
+        return AnimationInstance(self, pos)
+
+
+class CharacterAnimation(Animation):
+    def create_instance(self, character_name, direction, pos=(0,0)):
+        return CharacterAnimationInstance(character_name, direction, self, pos)
+
 
 
 class AnimationInstance:
-    def __init__(self, animation, character_name, pos=(0,0), direction='se'):
+    def __init__(self, animation, pos=(0,0)):
         self.animation = animation
-        self.name = character_name
         self.pos = pos
-        self.direction = direction
-        self.play('stand')
+        self.play('default')
         pyglet.clock.schedule(self.next_frame, self.animation.frame_rate)
 
     def play(self, sequence_name):
@@ -57,20 +62,53 @@ class AnimationInstance:
     def draw(self):
         """
         Current placeholder, used to send the appropriate image file to the actor being animated.
+
+        :return: str, path to correct image file for animation.
+        """
+        return os.path.join("images",
+                            "pc",
+                            "{action}{phase}.png".format(action=self.playing.replace('default', 'stand'),
+                                                         phase=self.sequence.frames[self.current_frame]))
+
+    def destroy(self):
+        pyglet.clock.unschedule(self.next_frame)
+
+
+class CharacterAnimationInstance(AnimationInstance):
+    def __init__(self, character_name, direction, *args, **kwargs):
+        super(CharacterAnimationInstance, self).__init__(*args, **kwargs)
+        self.name = character_name
+        self.direction = direction
+
+    def play(self, sequence_name):
+        """
+        Start the sequence for a specified animation.
+
+        Set current_frame to 0 to start.
+
+        :param sequence_name: str, animation we are playing
+        """
+        self.current_frame = 0
+        self.playing = sequence_name
+        self.sequence = self.animation.sequences[sequence_name]
+
+    def draw(self):
+        """
+        Current placeholder, used to send the appropriate image file to the actor being animated.
+
         :return: str, path to correct image file for animation.
         """
         return os.path.join("images",
                             "pc",
                             "{name}-{dir}-{action}{phase}.png".format(name=self.name,
                                                                       dir=self.direction,
-                                                                      action=self.playing,
+                                                                      action=self.playing.replace('default', 'stand'),
                                                                       phase=self.sequence.frames[self.current_frame]))
 
-    def destroy(self):
-        pyglet.clock.unschedule(self.next_frame)
 
-animated_character = Animation({
+animated_character = CharacterAnimation({
     'stand': Sequence(('',), loop),
+    'default': Sequence(('',), loop),
     'walk': Sequence((1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4), loop),  # hack, frame_rate isn't working for me.
     'shoot': Sequence(('',), loop)
 })
